d577f025bd81b3ac168c4271444cafd366623421
imilne <imilne@5e7781e8-d145-884b-bf32-ee02a10d3f06>
2006-11-15 10:27:10 +0000

    ReadSeq code added to version control (it had been forgotten!)

---
+++ trunk/src/topali/fileio/FileReadSeq.java
@@ -0,0 +1,57 @@
+// (C) 2003-2006 Iain Milne
+//
+// This package may be distributed under the
+// terms of the GNU General Public License (GPL)
+
+package topali.fileio;
+
+import java.io.*;
+import java.util.*;
+
+import topali.data.*;
+import topali.gui.*;
+
+import doe.*;
+
+import sbrn.commons.bioinf.ReadSeq;
+
+// Unlike the other classes, this one just pretends to be a file-format handler,
+// delegating the actual work to the ReadSeq util class (web/cgi service)
+class FileReadSeq extends FileGeneric
+{
+	FileReadSeq(SequenceSet s)
+	{
+		ss = s;
+	}
+	
+	public boolean readFile(File file)
+	{
+		File outFile = new File(Prefs.tmpDir, "tmpAlignment");
+		
+		// Use ReadSeq to convert the file into a FASTA formatted file
+		System.out.println("Attempting ReadSeq conversion...");
+		ReadSeq readSeq = new ReadSeq(file, outFile);
+		try { readSeq.convertFile(); }
+		catch (Exception e)
+		{
+			System.out.println(e);
+			success = false;
+			ss.reset();
+			
+			MsgBox.msg("TOPALi was unable to convert the file using ReadSeq:\n"
+				+ e, MsgBox.ERR);
+		}
+		
+		// Then use TOPALi's FASTA handler to load the file
+		FileFasta ff = new FileFasta(ss);
+		success = ff.readFile(outFile);
+		
+		return success;
+	}
+	
+	public void writeFile(File file, int[] index, int start, int end, boolean useSafeNames)
+		throws IOException
+	{
+		
+	}
+}
