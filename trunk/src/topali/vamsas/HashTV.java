c3098da88f40af0da40bd9a1db36d03db57be786
imilne <imilne@5e7781e8-d145-884b-bf32-ee02a10d3f06>
2007-01-24 08:47:28 +0000

    Updated vamsas code.

--- trunk/src/topali/vamsas/HashTV.java
+++ trunk/src/topali/vamsas/HashTV.java
@@ -2,6 +2,8 @@ package topali.vamsas;
 
 import java.util.*;
 
+import uk.ac.vamsas.objects.core.*;
+
 public class HashTV
 {
 	// Holds mappings between TOPALi data objects and their associated VAMSAS
@@ -9,4 +11,40 @@ public class HashTV
 	private Hashtable<Object, String> ht = new Hashtable<Object, String>();
 	
 	
+	public void addReference(Object obj, String id)
+	{
+		ht.put(obj, id);
+	}
+	
+	public String getVorbaID(Object obj)
+	{
+		if (ht.containsKey(obj))
+		{
+			return ht.get(obj);
+		}
+		else
+		{
+			return null;
+		}
+	}
+	
+	public AlignmentSequence getAlignmentSequence(Object tRef)
+	{
+		if (ht.containsKey(tRef))
+		{
+			return null;
+		}
+		else
+		{
+			AlignmentSequence v = new AlignmentSequence();
+//			addReference(tRef, BUT CAN'T GET VORBA ID UNTIL ADDED TO DOC!?
+			return v;
+		}
+	}
+	
+	// TODO: getAlignmentSequence(TOPALiObjectRef)
+	//   gets string for vamsas obj and finds it in client doc
+	// OR
+	//   doesn't find it, so makes a new instance instead, adding the reference
+	//   to the hashtable at the same time
 }
---
+++ trunk/src/topali/vamsas/PickHandler.java
@@ -0,0 +1,47 @@
+package topali.vamsas;
+
+import javax.swing.*;
+
+import topali.gui.*;
+
+import uk.ac.vamsas.client.picking.*;
+
+public class PickHandler implements IMessageHandler
+{
+	private IPickManager manager;
+		
+	public PickHandler(IPickManager manager)
+	{
+		this.manager = manager;
+		manager.registerMessageHandler(this);
+	}
+	
+	public void sendMessage(Message message)
+	{
+		manager.sendMessage(message);
+	}
+	
+	public void handleMessage(final Message message)
+	{
+		Runnable r = new Runnable() {
+			public void run() {
+				processMessage(message);
+			}
+		};
+		
+		SwingUtilities.invokeLater(r);
+	}
+	
+	private void processMessage(Message message)
+	{
+		if (message instanceof MouseOverMessage)
+		{
+			MouseOverMessage mom = (MouseOverMessage) message;
+			
+			String seqID = mom.getVorbaID();
+			int position = mom.getPosition();
+			
+			TOPALi.winMain.vamsasMouseOver(seqID, position);
+		}
+	}
+}
