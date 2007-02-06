68cf5bda03e41ce9182f5f084b0a498e902b9f66
imilne <imilne@5e7781e8-d145-884b-bf32-ee02a10d3f06>
2007-02-06 14:10:34 +0000

    Added CodeMLModel.java to the topali.data.CodeMLResult class so that the data received back from each model run can be held in a proper OO structure.

---
+++ trunk/src/topali/data/CodeMLModel.java
@@ -0,0 +1,27 @@
+// (C) 2003-2006 Iain Milne
+//
+// This package may be distributed under the
+// terms of the GNU General Public License (GPL)
+
+package topali.data;
+
+/*
+ * Class that stores output data from a given (model's) codeML run.
+ */
+public class CodeMLModel
+{
+	public String runNumber;
+	public String name;
+	public String siteClass;
+	
+	public int params;
+	
+	public float dnDS;
+	public float w;
+	public float p0, p1, p2;
+	public float w0, w1, w2;
+	
+//	public float p, q, w;
+	
+	public float likelihood;
+}
