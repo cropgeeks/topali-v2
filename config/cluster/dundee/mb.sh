d10d3df306b12af24b3b79a3b1f8fbf5a8fe5296
Iain Milne <imilne@users.noreply.github.com>
2007-08-21 07:40:46 +0000

    Fixed the naming of the SGE script for a MrBayes analysis (and the name of the class that is called by the script).

--- config/cluster/dundee/mb.sh
+++ config/cluster/dundee/mb.sh
@@ -5,9 +5,9 @@
 #$ -l mem_free=1500M
 #$ -l h_cpu=01:00:00
 
-#$ -N TPi-MBT
+#$ -N TPi-MB
 #$ -cwd
 #$ -j y
 
 hostname
-$JAVA -Xmx256m -cp $TOPALi topali.cluster.jobs.trees.MBTreeAnalysis "$JOB_DIR"
+$JAVA -Xmx256m -cp $TOPALi topali.cluster.jobs.mrbayes.MrBayesAnalysis "$JOB_DIR"
