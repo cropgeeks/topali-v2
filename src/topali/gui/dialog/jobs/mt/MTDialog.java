4cd55196b32eab875ddf5304017ad4c84f2309df
Iain Milne <imilne@users.noreply.github.com>
2007-11-09 16:12:28 +0000

    Model generator dialog now appears at the correct position (aligned to the main window).

--- src/topali/gui/dialog/jobs/mt/MTDialog.java
+++ src/topali/gui/dialog/jobs/mt/MTDialog.java
@@ -19,41 +19,41 @@ public class MTDialog extends JDialog implements ActionListener
 
 	MTDialogPanel panel;
 	private JButton bRun = new JButton(), bCancel = new JButton(), bDefault = new JButton(), bHelp = new JButton();
-	
+
 	ModelTestResult res;
 	AlignmentData data;
 	SequenceSet ss;
-	
+
 	public MTDialog(WinMain winMain, AlignmentData data, ModelTestResult res) {
 		super(winMain, "Model Selection", true);
-		
+
 		this.data = data;
 		this.ss = data.getSequenceSet();
 		this.res = res;
 		init();
-		
-		setLocationRelativeTo(winMain);
+
 		pack();
+		setLocationRelativeTo(winMain);
 	}
-	
+
 	public ModelTestResult getResult() {
 		return this.res;
 	}
-	
+
 	private void init() {
 		this.setLayout(new BorderLayout());
-		
+
 		panel = new MTDialogPanel(this.res, ss.isDNA());
 		add(panel, BorderLayout.CENTER);
-		
+
 		JPanel bp = Utils.getButtonPanel(bRun, bCancel, bDefault, bHelp, this, "modelselection");
 		add(bp, BorderLayout.SOUTH);
 
 		getRootPane().setDefaultButton(bRun);
 		Utils.addCloseHandler(this, bCancel);
 	}
-	
-	
+
+
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
@@ -62,7 +62,7 @@ public class MTDialog extends JDialog implements ActionListener
 			this.res = panel.getResult();
 			this.res.isRemote = remote;
 			this.res.selectedSeqs = ss.getSelectedSequenceSafeNames();
-			
+
 			int runNum = data.getTracker().getMtRunCount() + 1;
 			data.getTracker().setMtRunCount(runNum);
 			this.res.guiName = "Model Selection " + runNum;
